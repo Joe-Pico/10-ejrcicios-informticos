7fe62efb28b86a7fc26a577f9becd4cf6c83b50e
Joe-Pico <jpico@alumno.montepiedra.edu.ec>
2025-10-06 09:12:11 -0500

    Add files via upload

---
+++ principal (4).cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+
+    cout << "Ingrese un número para ver su tabla de multiplicar: ";
+    cin >> numero;
+
+    cout << "Tabla de multiplicar del " << numero << ":" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << numero << " x " << i << " = " << numero * i << endl;
+    }
+
+    return 0;
+}
